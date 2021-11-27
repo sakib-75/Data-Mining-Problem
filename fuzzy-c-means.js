48d7be997ae2d363768d10d3325f9fd684f87192
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2021-11-27 21:46:32 +0600

    first commit

---
+++ fuzzy-c-means.js
@@ -0,0 +1,171 @@
+$('#addDataClusterInp').click(function () {
+    let clusterAmount = $("#cluster-amount").val();
+    let dataPointAmount = $("#dataPoint-amount").val();
+    let valueOfm = $("#value-of-m").val();
+    let parent = $('#createFuzzyCInput');
+    parent.empty();
+
+    if (clusterAmount != '' && dataPointAmount != '' && valueOfm != '') {
+        if (dataPointAmount == clusterAmount) {
+
+            for (i = 0; i < dataPointAmount; i++) {
+                $('<textarea />').attr({
+                    class: 'input level-inp text-area',
+                    id: `datapoint-${i}`,
+                    placeholder: `Data point ${i+1}`
+                }).appendTo(parent);
+            }
+
+            for (i = 0; i < clusterAmount; i++) {
+                $('<textarea />').attr({
+                    class: 'input level-inp text-area',
+                    id: `cluster-${i}`,
+                    placeholder: `Cluster ${i+1}`
+                }).appendTo(parent);
+            }
+
+            $(".calculateBtn").show();
+
+        } else {
+            alert('Data pint and cluster amount should be same');
+        }
+    } else {
+        alert('Please enter all field');
+    }
+
+});
+
+
+
+
+
+$('#fuzzycCal').click(function () {
+    let clusterAmount = $("#cluster-amount").val();
+    let valueOfm = $("#value-of-m").val();
+    let clusterData = [],
+        dataPointValue = [];
+
+    for (i = 0; i < clusterAmount; i++) {
+        let cluster = $(`#cluster-${i}`).val(); // Each cluster data
+        clusterData[i] = cluster.split(",").map((num) => {
+            return +num;
+        });
+
+        let datapoint = $(`#datapoint-${i}`).val(); // Each data point value
+        dataPointValue[i] = datapoint.split(",").map((num) => {
+            return +num;
+        });
+
+    }
+
+    //Step 1 : Find centroid
+    let centroidPart = [],
+        centroidPart1 = 0,
+        centroidPart2 = 0;
+
+    $('#centroid-result').addClass('result');
+    let centroidresultpart1 = $('#centroid-result-part1');
+    centroidresultpart1.empty();
+
+    for (i = 0; i < clusterAmount; i++) { // Cluster loop [0,1,2...]
+        centroidresultpart1.append(`<b class='centroid-title'>Centroid ${i+1}: </b><br><br>`)
+
+        for (d = 0; d < clusterAmount; d++) { // Claster inside part
+
+            for (j = 0; j < clusterData[d].length; j++) { // Data loop [0,1,2...]
+                let x = dataPointValue[d][j] * Math.pow(clusterData[i][j], valueOfm);
+                centroidPart1 += x;
+
+                let y = Math.pow(clusterData[i][j], valueOfm);
+                centroidPart2 += y;
+
+            }
+
+            let centroidPoartCal = (centroidPart1 / centroidPart2);
+            centroidresultpart1.append(`${centroidPart1.toFixed(4)} &div; ${centroidPart2.toFixed(4)} = ${centroidPoartCal} <br><br>`);
+            $('.fade-img').fadeIn();
+
+            centroidPart.push(centroidPoartCal);
+            centroidPart1 = centroidPart2 = 0;
+
+        }
+    }
+
+});
+
+
+
+
+//................................................................
+
+$('#addClusterInp').click(function () {
+    let clusterAmount = $("#cluster-amount2").val();
+    let valueOfm = $("#value-of-m").val();
+    let parent = $('#cluster-input');
+    parent.empty();
+
+    if (clusterAmount != '' && valueOfm != '') {
+
+        for (i = 0; i < clusterAmount; i++) {
+            $('<input>').attr({
+                class: 'input level-inp',
+                id: `cluster-date-${i}`,
+                placeholder: `Cluster ${i+1} value`
+            }).appendTo(parent);
+        }
+        $(".calculateBtn").show();
+
+    } else {
+        alert('Please enter all field');
+    }
+
+});
+
+
+$('#updateMemberValue').click(function () {
+    let clusterAmount = $("#cluster-amount2").val();
+    let valueOfm = $("#value-of-m2").val();
+    let upper, bottom, bottomSum, updatedData, first_value, other_value, power;
+    power = (1 / (valueOfm - 1));
+
+    first_value = $(`#cluster-date-0`).val();
+    upper = Math.pow((1 / first_value), power);
+    bottomSum = upper;
+
+    // Result
+    $('#upd-result').addClass('result');
+    let updatedresult1 = $('#updated-result1');
+    let updatedresult2 = $('#updated-result2');
+    let finalupdatedvalue = $('#final-updated-value');
+    updatedresult1.empty();
+    updatedresult2.empty();
+    finalupdatedvalue.empty();
+
+    $('#title').append('Updated membership value');
+    let sup = `<sup>(1/(${valueOfm}-1))</sup>`;
+    let sup2 = `<sup>${power.toFixed(2)}</sup>`
+    updatedresult1.append(`=> ((1/${first_value})${sup}  &div; ((1/${first_value})${sup} + `);
+    updatedresult2.append(`=> ((1/${first_value})${sup2}  &div; ((1/${first_value})${sup2} + `);
+
+    for (i = 1; i < clusterAmount; i++) {
+        other_value = $(`#cluster-date-${i}`).val();
+        bottom = Math.pow((1 / other_value), power);
+        bottomSum += bottom;
+
+        //Result
+        if (i == (clusterAmount - 1)) {
+            updatedresult1.append(`(1/${other_value})${sup}))`);
+            updatedresult2.append(`(1/${other_value})${sup2}))`);
+        } else {
+            updatedresult1.append(`(1/${other_value})${sup} + `);
+            updatedresult2.append(`(1/${other_value})${sup2} + `);
+        }
+
+    }
+
+    updatedData = (upper / bottomSum);
+    finalupdatedvalue.append(`<b>Updated Value: ${updatedData}</b>`);
+    $('.fade-img').fadeIn();
+
+
+});
---
+++ java-script.js
@@ -0,0 +1,227 @@
+$('#addEntropyInp').click(function () {
+
+    let featureNum = $('#feature-amount').val();
+    let parent = $('#createInput');
+    parent.empty();
+
+    if (featureNum != '') {
+        for (i = 0; i < featureNum; i++) {
+            $('<input>').attr({
+                type: 'number',
+                class: 'input level-inp',
+                id: `level-${i}`,
+                placeholder: `Amount of level ${i+1}`
+            }).appendTo(parent);
+        }
+        $(".calculateBtn").show();
+
+    } else {
+        alert('Please enter all field');
+    }
+
+});
+
+
+$('#entropyCalc').click(function () {
+    let totalData = $('#data-amount').val();
+    let tFeatureAmount = $('#feature-amount').val();
+    let levelProbability = [],
+        forLevel = [],
+        entropy = 0;
+    let isEmpty = inputEmptyCheck(tFeatureAmount);
+
+    if (isEmpty == false) {
+        $('#result').addClass('result');
+        let result1 = $('#result1');
+        let result2 = $('#result2');
+        let finalResult = $('#final-result');
+
+        result1.empty();
+        result2.empty();
+        finalResult.empty();
+        result1.append('=> -(');
+        result2.append('=> -(');
+
+        for (i = 0; i < tFeatureAmount; i++) {
+            let levelAmount = $(`#level-${i}`).val();
+            levelProbability[i] = levelAmount / totalData;
+            forLevel[i] = (levelProbability[i] * Math.log2(levelProbability[i]));
+            entropy -= forLevel[i];
+
+            if (i < (tFeatureAmount - 1)) {
+                result1.append(`(${levelAmount}/${totalData} &#215; log<sub>2</sub>(${levelAmount}/${totalData})) + `);
+                result2.append(`(${forLevel[i].toFixed(4)}) + `);
+
+            } else {
+                result1.append(`(${levelAmount}/${totalData} &#215; log<sub>2</sub>(${levelAmount}/${totalData})))`);
+                result2.append(`(${forLevel[i].toFixed(4)}))`);
+            }
+        }
+        //Final result
+        finalResult.append(`<b>Final Result (Entropy) = ${entropy}</b>`);
+
+    } else {
+        alert('Please enter all (' + tFeatureAmount + ') target feature levels')
+    }
+
+});
+
+
+function inputEmptyCheck(n) {
+    let count = 0;
+    for (i = 0; i < n; i++) {
+        if ($(`#level-${i}`).val() == '') {
+            count += 1;
+        }
+    }
+
+    if (count > 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+//............................................
+
+$('#addReminderInp').click(function () {
+
+    let totalData = $('#total-data').val();
+    let featureNum = $('#ds-feature-amount').val(); //input - amount of feature
+    let featureSubNum = $('#ds-feature-sub-amount').val(); //textbox - amount of sub-feature
+    let parent = $('#createRemInput');
+    parent.empty();
+
+    let featureSub = featureSubNum.split(",").map((num) => { //feature sub amount array
+        return +num;
+    });
+    let inputCheck = levelInputCheck(featureNum, featureSub);
+
+    if (totalData != '' && featureNum != '' && inputCheck == true) {
+        for (i = 0; i < featureNum; i++) {
+            $('<input>').attr({
+                type: 'number',
+                class: 'input level-inp',
+                id: `level-${i}`,
+                placeholder: `Amount of level ${i+1}`
+            }).appendTo(parent);
+
+            for (j = 0; j < featureSub[i]; j++) {
+                $('<input>').attr({
+                    type: 'number',
+                    class: 'input sub-level-inp',
+                    id: `sub-level-${i}-${j}`,
+                    placeholder: `Sub level ${j+1}`
+                }).appendTo(parent);
+            }
+        }
+        $(".calculateBtn").show();
+
+    } else {
+        alert('Please enter all field(1,2,3). Descriptive feature (field 2) & sub levels (field 3) should be match and every value of field 3 must be grater than 0');
+    }
+});
+
+
+
+$('#reminderCalc').click(function () {
+
+    let entropy = $('#entropy-value').val();
+    let totalData = $('#total-data').val();
+    let dsFeatureAmount = $('#ds-feature-amount').val();
+    let featureSubNum = $('#ds-feature-sub-amount').val();
+    let featureSub = featureSubNum.split(",").map((num) => { //feature sub amount array
+        return +num;
+    });
+
+    let levelProbability = [],
+        forLevel = [],
+        subLevelProbability = [],
+        forSubLevel,
+        prEntropy = 0,
+        reminder = 0,
+        ig;
+
+    $('#rm-result').addClass('result');
+    let result1 = $('#rm-result1');
+    let result2 = $('#rm-result2');
+    let result3 = $('#rm-result3');
+    let finalResult = $('#rm-final-result');
+
+    result1.empty();
+    result2.empty();
+    result3.empty();
+    finalResult.empty();
+    result1.append('&#10132; ');
+    result2.append('&#10132; ');
+    result3.append('&#10132; ');
+
+    for (i = 0; i < dsFeatureAmount; i++) {
+
+        let levelAmount = $(`#level-${i}`).val();
+        levelProbability[i] = levelAmount / totalData;
+        //..................................result..........................
+        let firstResult = 0;
+        let result_12_c1 = `(${levelAmount}/${totalData} &#215; (-(`;
+        let result_12_c2 = `<br> + (${levelAmount}/${totalData} &#215; (-(`;
+
+        if (firstResult == i) {
+            result1.append(result_12_c1);
+            result2.append(result_12_c1);
+            result3.append(`(${levelAmount}/${totalData} &#215; `);
+        } else {
+            result1.append(result_12_c2);
+            result2.append(result_12_c2);
+            result3.append(` + (${levelAmount}/${totalData} &#215; `);
+        }
+
+        for (j = 0; j < featureSub[i]; j++) {
+
+            let subLevelAmount = $(`#sub-level-${i}-${j}`).val();
+            subLevelProbability[j] = subLevelAmount / levelAmount;
+            forSubLevel = (subLevelProbability[j] * Math.log2(subLevelProbability[j]));
+            if (isNaN(forSubLevel)) forSubLevel = 0;
+            prEntropy -= forSubLevel;
+            //...........................................................result...................................................
+            if (j < (featureSub[i] - 1)) {
+                result1.append(`(${subLevelAmount}/${levelAmount} &#215; log<sub>2</sub>(${subLevelAmount}/${levelAmount})) + `);
+                result2.append(`(${forSubLevel.toFixed(4)}) + `);
+
+            } else {
+                result1.append(`(${subLevelAmount}/${levelAmount} &#215; log<sub>2</sub>(${subLevelAmount}/${levelAmount})))))`);
+                result2.append(`(${forSubLevel.toFixed(4)}))))`);
+            }
+        }
+
+        forLevel[i] = levelProbability[i] * prEntropy;
+        reminder += forLevel[i];
+        
+        //.................result...................
+        result3.append(`(${prEntropy.toFixed(4)}))`);
+        //set previous value to 0
+        prEntropy = 0;
+        firstResult = 1;
+
+    }
+    
+    ig = entropy - reminder;
+    //..........................Finall result..........................
+    finalResult.append(`<b>Reminder = ${reminder}</b>`);
+    $('#ig-result').append(`<b>Information Gain = (${entropy} - (${reminder.toFixed(4)})) <br> => ${ig} </b>`);
+
+});
+
+
+function levelInputCheck(dsF, subF) {
+    if (dsF == subF.length) {
+        if (subF.includes(0) == true) {
+            return false;
+        } else {
+            return true;
+        }
+
+    } else {
+        return false;
+    }
+}
