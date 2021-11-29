42c977c2d66a360b246fe000ce433fe8d48d6f9e
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2021-11-30 02:10:34 +0600

    Update fuzzy-c-means.js

--- fuzzy-c-means.js
+++ fuzzy-c-means.js
@@ -172,7 +172,7 @@ $('#updateMemberValue').click(function () {
     }
 
     updatedData = (upper / bottomSum);
-    finalupdatedvalue.append(`<b>Updated Value: ${updatedData.toFixed(2)}</b>`);
+    finalupdatedvalue.append(`<b>Updated Value (µ): ${updatedData.toFixed(2)}</b>`);
     $('.fade-img').fadeIn();
 
 
